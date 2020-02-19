2b43054f98a49998c20f357a27a77a6a2c38a33b
Troy Lee <troylee3006@tamu.edu>
2020-02-19 11:45:11 -0600

    Buttons are finished and working for the time being

--- src/buttonsAndSuch.js
+++ src/buttonsAndSuch.js
@@ -65,60 +65,105 @@ class ButtonPrimative{
 
 // button which is made using HTML
 class ButtonHTML{
+    // TODO: commented out so that can redo with only percentage coords
+    // // creates a button, from initialization to HTML creation
+    // constructor(xTopLeft, yTopLeft, widthButton, heightButton, isPercentage, buttonText, parentElement, documentButton, buttonFunc){
+    //     // alert("Beginning of ButtonHTML constructor");
+    //     // initialize variables
+    //     // given percentage coords (0-1)
+    //     if(isPercentage) {
+    //         this.xTopLeft = Math.floor(xTopLeft * parentElement.offsetWidth);
+    //         this.yTopLeft = Math.floor(yTopLeft * parentElement.offsetHeight);
+    //         this.widthButton = Math.floor(widthButton * parentElement.offsetWidth);
+    //         this.heightButton = Math.floor(heightButton * parentElement.offsetHeight);
+    //     }
+    //     // given absolute coords
+    //     else{
+    //         this.xTopLeft = xTopLeft;
+    //         this.yTopLeft = yTopLeft;
+    //         this.widthButton = widthButton;
+    //         this.heightButton = heightButton;
+    //     }
+    //     this.buttonText = buttonText;
+    //     this.myButtonFunc = buttonFunc;
+    //
+    //     // alert("Checkpoint 1 for buttonHTML");
+    //
+    //     // create HTML button
+    //     this.myButton = documentButton.createElement("button");
+    //     this.myButton.innerHTML = this.buttonText;
+    //
+    //     // alert("Checkpoint 2 for buttonHTML");
+    //
+    //     // append to document
+    //     parentElement.appendChild(this.myButton);
+    //
+    //     // alert("Checkpoint 3 for buttonHTML");
+    //
+    //     // set properties
+    //     // function
+    //     this.myButton.addEventListener("click", this.myButtonFunc);
+    //
+    //     // alert("Checkpoint 4 for buttonHTML");
+    //
+    //     // position
+    //     this.myButton.style.left = (this.xTopLeft).toString(10) + "px";
+    //     this.myButton.style.top = (this.yTopLeft).toString(10) + "px";
+    //     this.myButton.style.width = (this.widthButton).toString(10) + "px";
+    //     this.myButton.style.height = (this.heightButton).toString(10) + "px";
+    //
+    //     // alert("Checkpoint 5 for buttonHTML");
+    //
+    //     // so its "above" the canvas
+    //     this.myButton.style.position = "absolute";
+    //     this.myButton.style.zIndex = "2";
+    //
+    //     // TODO: padding? No. do it in css file
+    //
+    //     // alert("End of ButtonHTML constructor");
+    // }
+
     // creates a button, from initialization to HTML creation
     constructor(xTopLeft, yTopLeft, widthButton, heightButton, isPercentage, buttonText, parentElement, documentButton, buttonFunc){
-        // alert("Beginning of ButtonHTML constructor");
         // initialize variables
-        // given percentage coords (0-1)
-        if(isPercentage) {
-            this.xTopLeft = Math.floor(xTopLeft * parentElement.offsetHeight);
-            this.yTopLeft = Math.floor(yTopLeft * parentElement.offsetHeight);
-            this.widthButton = Math.floor(widthButton * parentElement.offsetHeight);
-            this.heightButton = Math.floor(heightButton * parentElement.offsetHeight);
-        }
-        // given absolute coords
-        else{
-            this.xTopLeft = xTopLeft;
-            this.yTopLeft = yTopLeft;
-            this.widthButton = widthButton;
-            this.heightButton = heightButton;
-        }
         this.buttonText = buttonText;
         this.myButtonFunc = buttonFunc;
+        this.xTopLeft = xTopLeft;
+        this.yTopLeft = yTopLeft;
+        this.widthButton = widthButton;
+        this.heightButton = heightButton;
+        this.isPercentage = isPercentage;
 
-        // alert("Checkpoint 1 for buttonHTML");
-
+        // HTML stuff
         // create HTML button
         this.myButton = documentButton.createElement("button");
         this.myButton.innerHTML = this.buttonText;
 
-        // alert("Checkpoint 2 for buttonHTML");
-
         // append to document
         parentElement.appendChild(this.myButton);
-
-        // alert("Checkpoint 3 for buttonHTML");
 
         // set properties
         // function
         this.myButton.addEventListener("click", this.myButtonFunc);
 
-        // alert("Checkpoint 4 for buttonHTML");
-
-        // position
-        this.myButton.style.left = this.xTopLeft;
-        this.myButton.style.top = this.yTopLeft;
-        this.myButton.style.width = this.widthButton;
-        this.myButton.style.height = this.heightButton;
-
-        // alert("Checkpoint 5 for buttonHTML");
-
         // so its "above" the canvas
         this.myButton.style.position = "absolute";
         this.myButton.style.zIndex = "2";
 
-        // TODO: padding? No. do it in css file
-
-        // alert("End of ButtonHTML constructor");
+        // position
+        // given percentage coords (0-1)
+        if(isPercentage) {
+            this.myButton.style.left = Math.round(this.xTopLeft * 100).toString(10) + "%";
+            this.myButton.style.top = Math.round(this.yTopLeft * 100).toString(10) + "%";
+            this.myButton.style.width = Math.round(this.widthButton * 100).toString(10) + "%";
+            this.myButton.style.height = Math.round(this.heightButton * 100).toString(10) + "%";
+        }
+        // given absolute coords
+        else{
+            this.myButton.style.left = (this.xTopLeft).toString(10) + "px";
+            this.myButton.style.top = (this.yTopLeft).toString(10) + "px";
+            this.myButton.style.width = (this.widthButton).toString(10) + "px";
+            this.myButton.style.height = (this.heightButton).toString(10) + "px";
+        }
     }
 }
